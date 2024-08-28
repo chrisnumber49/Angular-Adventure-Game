1e272a488ecefabf6b7c157bca1c53e19a06de73
Chrisnumber49 <h28925847@gmail.com>
2024-08-28 17:59:13 +0200

    initial environment set up and the route map constant

---
+++ src/app/type & const/routeMap.ts
@@ -0,0 +1,28 @@
+{
+  "routeMap": {
+    "text": "Do you want a doughnut?",
+    "options": [
+      {
+        "value": true,
+        "selected": false,
+        "nextStep": {
+          "text": "Do I deserve it?",
+          "options": [
+            {
+              
+            },{
+              
+            }
+          ]
+        }
+      },
+      {
+        "value": false,
+        "selected": false,
+        "nextStep": {
+          "text": "Maybe you want an apple"
+        }
+      }
+    ]
+  }
+}
